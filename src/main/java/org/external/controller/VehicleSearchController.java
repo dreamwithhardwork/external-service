f134090ac3820a2458b5b60b6c33935c36bb7ff6
srikanth <srikanth2121098@gmail.com>
2020-10-17 22:21:59 +0530

    add status filter to search controller

--- src/main/java/org/external/controller/VehicleSearchController.java
+++ src/main/java/org/external/controller/VehicleSearchController.java
@@ -33,33 +33,33 @@ public class VehicleSearchController {
 
 
     @GetMapping("/color/{color}")
-    List<Vehicle> getAllCarsByColor(@PathVariable String color){
+    List<Vehicle> getAllCarsByColor(@PathVariable String color, @RequestParam("status") Optional<String> status){
         return searchRepository.findByColorAndAutomobileType(color,type);
     }
 
     @GetMapping("/model/{model}")
-    List<Vehicle> getAllCarsByModel(@PathVariable String model){
+    List<Vehicle> getAllCarsByModel(@PathVariable String model,@RequestParam("status") Optional<String> status){
         return searchRepository.findByModelAndAutomobileType(model,type);
     }
 
     @GetMapping("/year/{year}")
-    List<Vehicle> getAllCarsByYear(@PathVariable Integer year){
+    List<Vehicle> getAllCarsByYear(@PathVariable Integer year,@RequestParam("status") Optional<String> status){
         return searchRepository.findByMakeYearAndType(year,type);
     }
 
     @GetMapping("/make/{make}")
-    List<Vehicle> getAllCarsByMake(@PathVariable String make){
+    List<Vehicle> getAllCarsByMake(@PathVariable String make,@RequestParam("status") Optional<String> status){
         return searchRepository.findByMakeAndAutomobileType(make,type);
     }
 
     @GetMapping("/mileage")
-    List<Vehicle> getAllCarsByMileage(@RequestParam("from") Integer from, @RequestParam("to") Integer to){
+    List<Vehicle> getAllCarsByMileage(@RequestParam("from") Integer from, @RequestParam("to") Integer to,@RequestParam("status") Optional<String> status){
         to = to==null?2022:to;
         return searchRepository.findByMileageBetweenAndAutomobileType(from,to,type);
     }
 
     @GetMapping("/price")
-    List<Vehicle> getAllCarsByPrice(@RequestParam("from") Float from, @RequestParam("to") Float to){
+    List<Vehicle> getAllCarsByPrice(@RequestParam("from") Float from, @RequestParam("to") Float to,@RequestParam("status") Optional<String> status){
         to = to == null?Float.MAX_VALUE:to;
         return searchRepository.findByPriceBetweenAndAutomobileType(from,to,type);
     }
@@ -68,7 +68,8 @@ public class VehicleSearchController {
     List<Vehicle> getAllCarsByFilter(@RequestParam("color") Optional<String> color, @RequestParam("year") Optional<String> year,
                                      @RequestParam("make") Optional<String> make, @RequestParam("model") Optional<String> model,
                                      @RequestParam("fromPrice") Optional<String> minPrice, @RequestParam("toPrice") Optional<String> maxPrice ,
-                                     @RequestParam("fromMileage") Optional<String> minMileage, @RequestParam("toMileage") Optional<String> maxMileage){
+                                     @RequestParam("fromMileage") Optional<String> minMileage, @RequestParam("toMileage") Optional<String> maxMileage
+                                      ,@RequestParam("status") Optional<String> status){
         Map<String,String> filter = new HashMap<>();
         filter.put("color",color.orElse(null));
         filter.put("year",year.orElse(null));
@@ -78,6 +79,7 @@ public class VehicleSearchController {
         filter.put("maxPrice",maxPrice.orElse(null));
         filter.put("minMileage",minMileage.orElse(null));
         filter.put("maxMileage",maxMileage.orElse(null));
+        filter.put("status",status.orElse(null));
         filter.put("automobileType","CAR");
 
         return searchFilterRepository.getVehiclesByFilter(filter);
