8ed8994c9f873ce04f7a5d3c5a5a53bbd7c49708
srikanth <srikanth2121098@gmail.com>
2020-10-18 12:04:14 +0530

    gradle upgrade

--- src/main/java/org/external/controller/VehicleSearchController.java
+++ src/main/java/org/external/controller/VehicleSearchController.java
@@ -7,10 +7,7 @@ import org.models.core.domain.report.AutomobileType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/vehicle")
@@ -23,54 +20,66 @@ public class VehicleSearchController {
     @Autowired
     SearchFilterRepository searchFilterRepository;
 
-    private static final AutomobileType type = AutomobileType.CAR;
+    private AutomobileType type = AutomobileType.CAR;
 
 
     @GetMapping("/all")
-    List<Vehicle> getAllCars(){
-        return searchRepository.findByAutomobileType(type);
+    List<Vehicle> getAllVehicles(@RequestParam("type") Optional<AutomobileType> type){
+        List<Vehicle> response= new ArrayList<>();
+        if(type.isEmpty()){
+            response.addAll(searchRepository.findByAutomobileType(AutomobileType.BIKE));
+            response.addAll(searchRepository.findByAutomobileType(AutomobileType.CAR));
+        }
+        else{
+            response.addAll(searchRepository.findByAutomobileType(type.get()));
+        }
+        return response;
     }
 
 
     @GetMapping("/color/{color}")
-    List<Vehicle> getAllCarsByColor(@PathVariable String color, @RequestParam("status") Optional<String> status){
+    List<Vehicle> getAllByColor(@PathVariable String color, @RequestParam("status") Optional<String> status,@RequestParam("type") AutomobileType type){
         return status.isEmpty()?searchRepository.findByColorAndAutomobileType(color,type):
                 searchRepository.findByColorAndAutomobileTypeAndStatus(color,type,status.get());
     }
 
     @GetMapping("/model/{model}")
-    List<Vehicle> getAllCarsByModel(@PathVariable String model,@RequestParam("status") Optional<String> status){
-        return searchRepository.findByModelAndAutomobileType(model,type);
+    List<Vehicle> getAllByModel(@PathVariable String model,@RequestParam("status") Optional<String> status, @RequestParam("type") AutomobileType type){
+        return status.isEmpty()? searchRepository.findByModelAndAutomobileType(model,type):
+                searchRepository.findByModelAndAutomobileTypeAndStatus(model,type,status.get());
     }
 
     @GetMapping("/year/{year}")
-    List<Vehicle> getAllCarsByYear(@PathVariable Integer year,@RequestParam("status") Optional<String> status){
-        return searchRepository.findByMakeYearAndType(year,type);
+    List<Vehicle> getAllByYear(@PathVariable Integer year,@RequestParam("status") Optional<String> status, @RequestParam("type") AutomobileType type){
+        return status.isEmpty()? searchRepository.findByMakeYearAndType(year,type):
+                searchRepository.findByMakeYearAndTypeAndStatus(year,type,status.get());
     }
 
     @GetMapping("/make/{make}")
-    List<Vehicle> getAllCarsByMake(@PathVariable String make,@RequestParam("status") Optional<String> status){
-        return searchRepository.findByMakeAndAutomobileType(make,type);
+    List<Vehicle> getAllByMake(@PathVariable String make,@RequestParam("status") Optional<String> status, @RequestParam("type") AutomobileType type){
+        return status.isEmpty()? searchRepository.findByMakeAndAutomobileType(make,type):
+                searchRepository.findByMakeAndAutomobileTypeAndStatus(make,type,status.get());
     }
 
     @GetMapping("/mileage")
-    List<Vehicle> getAllCarsByMileage(@RequestParam("from") Integer from, @RequestParam("to") Integer to,@RequestParam("status") Optional<String> status){
+    List<Vehicle> getAllCarsByMileage(@RequestParam("from") Integer from, @RequestParam("to") Integer to,@RequestParam("status") Optional<String> status,
+                                      @RequestParam("type") AutomobileType type){
         to = to==null?2022:to;
         return searchRepository.findByMileageBetweenAndAutomobileType(from,to,type);
     }
 
     @GetMapping("/price")
-    List<Vehicle> getAllCarsByPrice(@RequestParam("from") Float from, @RequestParam("to") Float to,@RequestParam("status") Optional<String> status){
+    List<Vehicle> getAllByPrice(@RequestParam("from") Float from, @RequestParam("to") Float to,@RequestParam("status") Optional<String> status, @RequestParam("type") AutomobileType type){
         to = to == null?Float.MAX_VALUE:to;
         return searchRepository.findByPriceBetweenAndAutomobileType(from,to,type);
     }
 
     @GetMapping("/filter")
-    List<Vehicle> getAllCarsByFilter(@RequestParam("color") Optional<String> color, @RequestParam("year") Optional<String> year,
+    List<Vehicle> getAllByFilter(@RequestParam("color") Optional<String> color, @RequestParam("year") Optional<String> year,
                                      @RequestParam("make") Optional<String> make, @RequestParam("model") Optional<String> model,
                                      @RequestParam("fromPrice") Optional<String> minPrice, @RequestParam("toPrice") Optional<String> maxPrice ,
                                      @RequestParam("fromMileage") Optional<String> minMileage, @RequestParam("toMileage") Optional<String> maxMileage
-                                      ,@RequestParam("status") Optional<String> status){
+                                      ,@RequestParam("status") Optional<String> status, @RequestParam("type") AutomobileType type){
         Map<String,String> filter = new HashMap<>();
         filter.put("color",color.orElse(null));
         filter.put("year",year.orElse(null));
@@ -81,8 +90,7 @@ public class VehicleSearchController {
         filter.put("minMileage",minMileage.orElse(null));
         filter.put("maxMileage",maxMileage.orElse(null));
         filter.put("status",status.orElse(null));
-        filter.put("automobileType","CAR");
-
+        filter.put("type",type.toString());
         return searchFilterRepository.getVehiclesByFilter(filter);
     }
 
