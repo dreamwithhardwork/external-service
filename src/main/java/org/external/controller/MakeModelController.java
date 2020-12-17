2533e481b7c872f5a5ac8fab7c48bdd44b9379ef
srikanth <srikanth2121098@gmail.com>
2020-12-17 23:12:22 +0530

    getvarian by name

--- src/main/java/org/external/controller/MakeModelController.java
+++ src/main/java/org/external/controller/MakeModelController.java
@@ -16,6 +16,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -62,6 +63,11 @@ public class MakeModelController {
     @GetMapping("/make-model")
     public Map<String, Map<String, Set<String>>> getMakeModelList(){
         return properties.getMakemodelvariants();
+    }
+
+    @GetMapping("/model/{name}")
+    public Model getModelByName(@PathVariable("name") String name){
+       return customRepositories.getModelByName(name);
     }
 
     @PutMapping
